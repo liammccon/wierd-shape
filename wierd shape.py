c0ef6960e63f58e49d1202906432abef964128e9
liammccon <liam.t.mccon@gmail.com>
2017-02-07 12:15:35 -0500

    Add files via upload

---
+++ wierd shape.py
@@ -0,0 +1,23 @@
+a='.'
+b='O'
+x=0
+y=0
+back=False
+z=-1
+w=20
+#Loops printing o and O. If x=y O is printed, else o is printed
+while True:
+    while (x<w):
+        if x==y:
+            print b,
+        if x!=y:
+            print a,
+        x=x+1
+    print ""    
+    x=0
+    y=y+1
+    if y==w:
+        y=0
+
+            
+    
